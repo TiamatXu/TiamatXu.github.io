af21088278e28dedcb7826854c4cf9abce1f45f7
TiamatXu <tiamat.xu@icloud.com>
2026-01-14 18:48:59 +0800

    feat: update header plugin typings, add markdown-it dependency, and fix SVG image attributes

--- .vitepress/headerMdPlugin.ts
+++ .vitepress/headerMdPlugin.ts
@@ -10,6 +10,7 @@
  * https://github.com/vitejs/docs-cn/tree/main/.vitepress/markdown-it-custom-anchor
  */
 
+// @ts-ignore
 import MarkdownIt from 'markdown-it'
 import { Header } from 'vitepress'
 
@@ -19,7 +20,7 @@ export interface AugmentedHeader extends Header {
 }
 
 export const headerPlugin = (md: MarkdownIt) => {
-  md.renderer.rules.heading_open = (tokens, i, options, env, self) => {
+  md.renderer.rules.heading_open = (tokens: { [x: string]: { children: any } }, i: number, options: any, env: any, self: { renderToken: (arg0: any, arg1: any, arg2: any) => any }) => {
     for (const child of tokens[i + 1].children!) {
       if (child.type === 'text' && child.content.endsWith('*')) {
         child.content = child.content.replace(/\s*\*+$/, '')
@@ -29,7 +30,7 @@ export const headerPlugin = (md: MarkdownIt) => {
   }
 
   const render = md.render
-  md.render = (content, env) => {
+  md.render = (content: any, env: { headers: AugmentedHeader[] }) => {
     const res = render(content, env)
 
     if (env && env.headers) {
