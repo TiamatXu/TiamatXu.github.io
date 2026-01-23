411ec71f3abecf203e029dda7b3a8e998b0f510e
TiamatXu <tiamat.xu@icloud.com>
2026-01-23 18:28:09 +0800

    feat(Home): change build time formatting to local time string
    Signed-off-by: TiamatXu <tiamat.xu@icloud.com>

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -9,7 +9,7 @@ const githubUsername = 'TiamatXu'
 // onMounted(load)
 
 const buildTime = import.meta.env.VITE_APP_BUILD_TIME
-const formattedBuildTime = buildTime ? new Date(buildTime).toUTCString() : 'Fetch failed with no build time.'
+const formattedBuildTime = buildTime ? new Date(buildTime).toTimeString() : 'Fetch failed with no build time.'
 </script>
 
 <template>
