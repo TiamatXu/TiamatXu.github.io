99c64cf47430e78e44f67c84f438b6f1cb0401de
TiamatXu <tiamat.xu@icloud.com>
2026-02-11 18:07:12 +0800

    chore(types): update env.d.ts and tsconfig.json for environment variables

--- env.d.ts
+++ env.d.ts
@@ -10,3 +10,11 @@ declare module '@vue/theme/highlight' {
   const createHighlighter: () => Promise<(input: string) => string>
   export default createHighlighter
 }
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_BUILD_TIME: string;
+}
