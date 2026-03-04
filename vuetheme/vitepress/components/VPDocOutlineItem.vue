bd51813e3eb4b5ac86a0316917fe53076f94bf05
TiamatXu <tiamat.xu@icloud.com>
2026-03-04 18:55:07 +0800

    fix: enhance outline item component to support dynamic nesting levels
    Signed-off-by: TiamatXu <tiamat.xu@icloud.com>

--- vuetheme/vitepress/components/VPDocOutlineItem.vue
+++ vuetheme/vitepress/components/VPDocOutlineItem.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { decode } from 'tiny-decode'
 import { useData } from 'vitepress'
 import type { MenuItemWithLinkAndChildren } from '../composables/outline.js'
 
-defineProps<{
+const props = defineProps<{
   headers: MenuItemWithLinkAndChildren[]
-  nested?: boolean
+  nested?: boolean | number
 }>()
 
-const { frontmatter } = useData()
+const { frontmatter, theme } = useData()
+const currentLevel = computed(() => typeof props.nested === 'number' ? props.nested : props.nested ? 3 : 2)
+const maxLevel = computed(() => {
+  const outline = frontmatter.value.outline ?? theme.value.outline
+  return outline === 'deep' ? 6 : typeof outline === 'number' ? outline : 2
+})
 
 function onClick({ target: el }: Event) {
   const id = '#' + (el as HTMLAnchorElement).href!.split('#')[1]
@@ -23,8 +29,8 @@ function onClick({ target: el }: Event) {
       <a class="outline-link" :href="link" @click="onClick" v-show="!hidden">
         {{ decode(text) }}
       </a>
-      <template v-if="children?.length && frontmatter.outline === 'deep'">
-        <VPDocOutlineItem :headers="children" :nested="true" />
+      <template v-if="children?.length && currentLevel < maxLevel">
+        <VPDocOutlineItem :headers="children" :nested="currentLevel + 1" />
       </template>
     </li>
   </ul>
